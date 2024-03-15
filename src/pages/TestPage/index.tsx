7f827fe9e995850ea3280f6528142e3c0ddfeb7d
dev <dev@devbetter.net>
2024-03-15 17:49:40 +0700

    aaaa

---
+++ src/pages/TestPage/index.tsx
@@ -0,0 +1,11 @@
+function TestPage()
+{
+    return (
+        <>
+            <h1>Hello Test Page</h1>
+            <h3>abc</h3>
+        </>
+    );
+}
+
+export default TestPage;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,17 +4,18 @@ import { AdminLayout, PublicLayout } from "../layouts";
 
 const Home = lazy(() => import("../pages/Home"));
 const Login = lazy(() => import("../pages/Login"));
-
+const TestPage = lazy(() => import("../pages/TestPage"));
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<PublicLayout />}>
                 <Route index element={<Suspense fallback={<>...</>}><Home /></Suspense>} />
                 <Route path="home" element={<Suspense fallback={<>...</>}><Home /></Suspense>} />
+                <Route path="test" element={<Suspense fallback={<>...</>}><TestPage /></Suspense>} />
             </Route>
             <Route path="/login" element={<Login />}/>
             <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<h1>About</h1>} />
+                <Route index element={<h1>Admin Dashboard</h1>} />
             </Route>
         </>
     )
