9bb956dee4deccf09c998dc7e58f49c04697cd97
dev <dev@devbetter.net>
2024-03-25 10:50:58 +0700

    Remove unused code and import Bulma CSS framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <>
+    </>
   );
 }
 
